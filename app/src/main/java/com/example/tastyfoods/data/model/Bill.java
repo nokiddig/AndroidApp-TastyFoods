d1bf20438dada5ffaf55062634cf9b89abdfb95f
Én <118596609+BuiThiAnhs@users.noreply.github.com>
2023-04-20 12:27:44 +0700

    Update datatypes for project

--- app/src/main/java/com/example/tastyfoods/data/model/Bill.java
+++ app/src/main/java/com/example/tastyfoods/data/model/Bill.java
@@ -1,4 +1,50 @@
 package com.example.tastyfoods.data.model;
 
 public class Bill {
+    private  String billID;
+    private String status;
+
+    private String dateTime;
+    private double totalMoney;
+
+    public String getBillID() {
+        return billID;
+    }
+
+    public void setBillID(String billID) {
+        this.billID = billID;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public double getTotalMoney() {
+        return totalMoney;
+    }
+
+    public void setTotalMoney(double totalMoney) {
+        this.totalMoney = totalMoney;
+    }
+
+    public Bill(String billID, String status, String dateTime, double totalMoney) {
+        this.billID = billID;
+        this.status = status;
+        this.dateTime = dateTime;
+        this.totalMoney = totalMoney;
+    }
+
+
 }
