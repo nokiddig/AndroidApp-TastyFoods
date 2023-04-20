0b707f1d81c34802873ef40c45d0248523f20001
pvh1806 <phamvanhung18060202@gmail.com>
2023-04-20 15:29:49 +0700

    Create View of ProductDetail

---
+++ app/src/main/java/com/example/tastyfoods/ViewModel/ProductDetailFragment.java
@@ -0,0 +1,25 @@
+package com.example.tastyfoods.ViewModel;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.tastyfoods.R;
+public class ProductDetailFragment extends Fragment {
+    public ProductDetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_product_detail, container, false);
+    }
+}
